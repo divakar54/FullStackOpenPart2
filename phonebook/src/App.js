483d0bcab390b7073c68c7292c5958fdfb5a854a
Divakar <divakar@gmail.com>
2020-11-19 01:02:49 +0530

    Added 2.11

--- phonebook/src/App.js
+++ phonebook/src/App.js
@@ -6,17 +6,7 @@ import axios from 'axios'
 
 const App = () => {
 
-<<<<<<< HEAD
   const [ persons, setPersons ] = useState([])
-=======
-  const [ persons, setPersons ] = useState([
-    { name: 'Arto Hellas', number: '040-123456' },
-    { name: 'Ada Lovelace', number: '39-44-5323523' },
-    { name: 'Dan Abramov', number: '12-43-234345' },
-    { name: 'Mary Poppendieck', number: '39-23-6423122' }
-    
-  ])
->>>>>>> a4e6ecea2ff801a0ba66eeb3e64cb5dcb6345e01
 
   const [ newName, setNewName ] = useState('')
 
