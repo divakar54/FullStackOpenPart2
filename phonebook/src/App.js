a4e6ecea2ff801a0ba66eeb3e64cb5dcb6345e01
divakar54 <divakar10d@gmail.com>
2020-11-18 10:43:42 +0530

    Update App.js

--- phonebook/src/App.js
+++ phonebook/src/App.js
@@ -10,6 +10,7 @@ const App = () => {
     { name: 'Ada Lovelace', number: '39-44-5323523' },
     { name: 'Dan Abramov', number: '12-43-234345' },
     { name: 'Mary Poppendieck', number: '39-23-6423122' }
+    
   ])
 
   const [ newName, setNewName ] = useState('')
